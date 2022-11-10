7a787af69a87946f76d231990ae19c275661cd57
chibatakashi <nixon0290211@icloud.com>
2022-11-11 06:36:48 +0900

    課題終了

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import webbrowser
 import pyautogui
 import pyperclip
 import time
+
 # GUIで
 webbrowser.open_new_tab("https://www.google.com/")
 time.sleep(1)
@@ -9,35 +10,9 @@ time.sleep(1)
 # pyautogui.click(left, top)
 # pyautogui.click(382, 269)
 pyperclip.copy("平泉町　観光")
-pyautogui.hotkey("Ctrl", "v")
-pyautogui.press("enter")
-time.sleep(2)
+pyautogui.keyDown("command")
+pyautogui.press("v")
+pyautogui.keyUp("command")
+pyautogui.press("Enter")
+time.sleep(3)
 pyautogui.screenshot("screenshot.png")
-
-# セレニウム
-# from selenium import webdriver
-# driver = webdriver.Chrome()
-
-
-
-
-
-
-
-
-# import webbrowser
-# import pyautogui
-# import pyperclip
-# import time
-
-
-# webbrowser.open_new("https://www.google.com")
-
-# time.sleep(3)
-# pyperclip.copy("平泉町 観光")
-# pyautogui.hotkey("command", "v")
-# pyautogui.press("enter")
-
-
-# time.sleep(5)
-# pyautogui.screenshot("screenshot_test_image.png")
